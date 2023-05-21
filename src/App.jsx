d051e1cb1dbeba582f8c61984853896ecafc023c
SergioRiatiga <sergioriatiga@gmail.com>
2023-05-20 19:20:12 -0500

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,28 @@ function App() {
 
   return (
     <div style={objStyle} className='app'>
+      <header>
+        <h1>GALLETA DE LA FORTUNA</h1>
+      </header>
+      <section className='content'>
+        <div className='text'>
+        <PhrasesCard phraseRandom={phraseRandom}/>
+        <img  className='quotes' src="../public/images/quote-alt-right-solid-24.png" alt="comillas" />
+        </div>
+        <div>
+        <ButtonPhrase 
+        setPhraseRandom={setPhraseRandom} 
+        setPathRandom={setPathRandom} 
+        arrBackground={arrBackground}
+        />
+        </div>
+      </section>
+
+      <footer className='contactInfo'>
+        <p>Sergio Andrés Riatiga Ibáñez, sergioriatiga@gmail.com, <a href="https://github.com/SergioRiatiga" target="_blank">github.</a></p>
+      </footer>
       
-      <h1>GALLETA DE LA FORTUNA</h1>
-      <div className='content'>
-      <PhrasesCard phraseRandom={phraseRandom}/>
-      <img  className='quotes' src="../public/images/quote-alt-right-solid-24.png" alt="comillas" />
-      </div>
-      <ButtonPhrase 
-      setPhraseRandom={setPhraseRandom} 
-      setPathRandom={setPathRandom} 
-      arrBackground={arrBackground}
-      />
-      <p className='contactInfo'>Sergio Andrés Riatiga Ibáñez, sergioriatiga@gmail.com, <a href="https://github.com/SergioRiatiga" target="_blank">github.</a></p>
+      
     </div>
 
   )
